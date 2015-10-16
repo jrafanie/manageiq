7176f6706626b1d7406c3e7b681474ec79b72b68
Joe Rafaniello <jrafanie@redhat.com>
2015-10-16 16:53:18 -0400

    Add live and young object counts.

--- gems/pending/util/ruby_gc_logger.rb
+++ gems/pending/util/ruby_gc_logger.rb
@@ -25,19 +25,21 @@ module RubyGCLogger
   private
 
   def gc_stat_line
-    [Time.now.iso8601] +
+    gc_stat       = GC.stat
+    live_objects  = gc_stat[:total_allocated_objects] - gc_stat[:total_freed_objects]
+    young_objects = live_objects - gc_stat[:old_objects]
+
+    [Time.now.iso8601, live_objects, young_objects, ObjectSpace.memsize_of_all] +
       MiqProcess.processInfo.values_at(*miq_process_keys) +
-      GC.stat.values_at(*gc_stat_keys) +
-      [ObjectSpace.memsize_of_all] +
+      gc_stat.values_at(*gc_stat_keys) +
       ObjectSpace.count_objects.values_at(*count_objects_keys) +
       ObjectSpace.count_objects_size.values_at(*count_objects_size_keys)
   end
 
   def gc_stat_header
-    [:time] +
+    [:time, :live_objects, :young_objects, :memsize_of_all] +
       miq_process_keys +
       gc_stat_keys +
-      [:memsize_of_all] +
       count_objects_keys +
       count_objects_size_keys.collect { |key| key.to_s.concat("_SIZE").to_sym }
   end
