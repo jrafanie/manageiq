bdca37d2277c12ff2a48ebb0625b8ee2eb736a26
Joe Rafaniello <jrafanie@redhat.com>
2016-08-03 10:29:46 -0400

    get container_summary_helper passing
    Note, I think spec/helpers/container_summary_helper.rb is incorrectly named
    because it will never run since default globbing of spec files uses:
    **/**_spec.rb.
    
    Nevertheless, this test included ApplicationHelper in the test class so I created
    two methods, one to stub as "allowed" and one to stub as "denied" and use some
    ugly magic to stub all the ApplicationHelper types:
    
    ObjectSpace.each_object(ApplicationHelper) { |klass| allow(klass).to receive(:role_allows).and_return(false) }
    
    UGH, it seems to work though.
    
    If we use that hack, we can eliminate lots of stubbing of things that might include
    ApplicationHelper.
    
    We're still RED.

--- spec/helpers/container_summary_helper.rb
+++ spec/helpers/container_summary_helper.rb
@@ -2,28 +2,26 @@ describe ContainerSummaryHelper do
   REL_HASH_WITH_LINK = [:label, :image, :value, :link, :title]
   REL_HASH_WITHOUT_LINK = [:label, :image, :value]
 
+  include ControllerSpecHelper
+
   let(:container_project) { FactoryGirl.create(:container_project) }
   before do
     self.class.send(:include, ApplicationHelper)
 
     @record = FactoryGirl.build(:container_group, :container_project => container_project)
-
-    login_as @user = FactoryGirl.create(:user)
   end
 
   context ".textual_container_project" do
     subject { textual_container_project }
 
     it 'show link when role allows' do
-      allow(@user).to receive(:role_allows?).and_return(true)
-
+      set_allowed_user_privileges
       expect(subject.keys).to eq(REL_HASH_WITH_LINK)
       expect(subject[:value]).to eq(container_project.name)
     end
 
     it 'hide link when role does not allow' do
-      allow(@user).to receive(:role_allows?).and_return(false)
-
+      set_denied_user_privileges
       expect(subject.keys).to eq(REL_HASH_WITHOUT_LINK)
       expect(subject[:value]).to eq(container_project.name)
     end
@@ -34,15 +32,13 @@ describe ContainerSummaryHelper do
     subject { textual_containers }
 
     it 'show link when role allows' do
-      allow(@user).to receive(:role_allows?).and_return(true)
-
+      set_allowed_user_privileges
       expect(subject.keys).to eq(REL_HASH_WITH_LINK)
       expect(subject[:value]).to eq("2")
     end
 
     it 'hide link when role does not allow' do
-      allow(@user).to receive(:role_allows?).and_return(false)
-
+      set_denied_user_privileges
       expect(subject.keys).to eq(REL_HASH_WITHOUT_LINK)
       expect(subject[:value]).to eq("2")
     end
--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -17,6 +17,35 @@ module ControllerSpecHelper
     allow(Rbac::Authorizer).to receive(:role_allows).and_return(true)
   end
 
+  # Refactor these two methods, toss out stubbing we don't need
+  def set_allowed_user_privileges
+    user = FactoryGirl.create(:user_with_group)
+    allow(User).to receive(:server_timezone).and_return("UTC")
+    allow_any_instance_of(described_class).to receive(:set_user_time_zone)
+
+    allow(controller).to receive(:check_privileges).and_return(true)
+    login_as user
+    allow(controller).to receive(:role_allows).and_return(true)
+
+    # blindly mock all things that include ApplicationHelper to return true from role_allows
+    # We should be able to get rid of lots of mocking if this thing works.
+    ObjectSpace.each_object(ApplicationHelper) { |klass| allow(klass).to receive(:role_allows).and_return(true) }
+    allow(Rbac::Authorizer).to receive(:role_allows).and_return(true)
+  end
+
+  def set_denied_user_privileges
+    user = FactoryGirl.create(:user_with_group)
+    allow(User).to receive(:server_timezone).and_return("UTC")
+    allow_any_instance_of(described_class).to receive(:set_user_time_zone)
+
+    allow(controller).to receive(:check_privileges).and_return(false)
+    login_as user
+    allow(controller).to receive(:role_allows).and_return(false)
+    ObjectSpace.each_object(ApplicationHelper) { |klass| allow(klass).to receive(:role_allows).and_return(false) }
+
+    allow(Rbac::Authorizer).to receive(:role_allows).and_return(false)
+  end
+
   def setup_zone
     EvmSpecHelper.create_guid_miq_server_zone
   end
