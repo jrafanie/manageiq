46bccf34fdfa2df963770d9cf171ed4d2a2eadae
Joe Rafaniello <jrafanie@gmail.com>
2023-10-17 10:51:41 -0400

    Warn when handling NameError and DisallowedClass

--- lib/extensions/yaml_load_aliases.rb
+++ lib/extensions/yaml_load_aliases.rb
@@ -4,13 +4,15 @@ module YamlLoadAliases
   # constants at that time, such as MiqExpression, Ruport, so we have two sets of permitted classes.
   def safe_load(*args, **kwargs)
     super(*args, **kwargs.reverse_merge(:aliases => true, :permitted_classes => DEFAULT_PERMITTED_CLASSES))
-  rescue NameError
+  rescue NameError => err
+    warn "WARNING: Trying Permitted Classes due to NameError: #{err}"
     super(*args, **kwargs.reverse_merge(:aliases => true, :permitted_classes => DEFAULT_PERMITTED_CLASSES + [MiqExpression, MiqReport, Ruport::Data::Table, Ruport::Data::Record]))
-  rescue Psych::DisallowedClass
+  rescue Psych::DisallowedClass => err
     # Temporary hack to fallback to psych 3 behavior to go back to unsafe load if it's a disallowed class.
     # See: https://stackoverflow.com/questions/71191685/visit-psych-nodes-alias-unknown-alias-default-psychbadalias/71192990#71192990
     # The alternative is to enumerate all the classes we will allow to be loaded from YAML, such as many of the various models.
-    unsafe_load(*args, **kwargs.slice(:filename, :fallback, :symbolize_names, :freeze))
+    warn "WARNING: Using fallback due to DisallowedClass: #{err}"
+    unsafe_load(*args, **(kwargs.slice(:filename, :fallback, :symbolize_names, :freeze)))
   end
 end
 
