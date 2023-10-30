2d15c21e4e59913f8aa73d7339ab3cca035bc60d
Joe Rafaniello <jrafanie@gmail.com>
2023-10-30 17:05:05 -0400

    Comment out the fallback logic and let it fail
    This lets us populate the permitted classes with actual values in test and dev.

--- lib/extensions/yaml_load_aliases.rb
+++ lib/extensions/yaml_load_aliases.rb
@@ -5,12 +5,12 @@ module YamlLoadAliases
     # permitted_classes kwarg is provided because rails 6.1.7.x expects it as a defined kwarg.  See: https://github.com/rails/rails/blob/9ab33753b6bab1809fc73d35b98a5c1d0c96ba1b/activerecord/lib/active_record/coders/yaml_column.rb#L52
     permitted_classes += YamlPermittedClasses.initialized? ? YamlPermittedClasses.app_yaml_permitted_classes : YamlPermittedClasses.default_permitted_classes
     super(yaml, permitted_classes: permitted_classes, aliases: true, **kwargs)
-  rescue Psych::DisallowedClass => err
-    # Temporary hack to fallback to psych 3 behavior to go back to unsafe load if it's a disallowed class.
-    # See: https://stackoverflow.com/questions/71191685/visit-psych-nodes-alias-unknown-alias-default-psychbadalias/71192990#71192990
-    # The alternative is to enumerate all the classes we will allow to be loaded from YAML, such as many of the various models.
-    warn "WARNING: Using fallback to unsafe_load due to DisallowedClass: #{err}"
-    unsafe_load(yaml, **(kwargs.except(:aliases, :permitted_classes, :permitted_symbols)))
+  # rescue Psych::DisallowedClass => err
+  #   # Temporary hack to fallback to psych 3 behavior to go back to unsafe load if it's a disallowed class.
+  #   # See: https://stackoverflow.com/questions/71191685/visit-psych-nodes-alias-unknown-alias-default-psychbadalias/71192990#71192990
+  #   # The alternative is to enumerate all the classes we will allow to be loaded from YAML, such as many of the various models.
+  #   warn "WARNING: Using fallback to unsafe_load due to DisallowedClass: #{err}"
+  #   unsafe_load(yaml, **(kwargs.except(:aliases, :permitted_classes, :permitted_symbols)))
   end
 end
 
