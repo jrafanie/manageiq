af3ed3fe987d0b94512ae4e4a26ac553855f9086
Joe Rafaniello <jrafanie@gmail.com>
2023-11-17 10:04:06 -0500

    Add Vim classes we use fairly often to the list

--- lib/yaml_permitted_classes.rb
+++ lib/yaml_permitted_classes.rb
@@ -1,7 +1,7 @@
 class YamlPermittedClasses
   DEFAULT_PERMITTED_CLASSES = [Object, Range, Regexp, Symbol, Date, Time, DateTime, ActiveSupport::Duration, ActiveSupport::HashWithIndifferentAccess, ActiveSupport::TimeWithZone, ActiveSupport::TimeZone]
   def self.app_yaml_permitted_classes
-    @app_yaml_permitted_classes ||= DEFAULT_PERMITTED_CLASSES + [MiqExpression, MiqReport, Ruport::Data::Table, Ruport::Data::Record, User, ConfigurationScript, ContainerImage, ContainerTemplate, OpenStruct, OrchestrationTemplate, ManageIQ::Providers::Vmware::InfraManager, ManageIQ::Providers::InfraManager::Vm, ActiveModel::Type::String, ActiveModel::Attribute.const_get(:FromDatabase), ActiveModel::Attribute.const_get(:FromUser), ActiveRecord::Type::Serialized, ActiveRecord::Type::Text, ActiveRecord::Coders::YAMLColumn, ActiveModel::Type::Integer]
+    @app_yaml_permitted_classes ||= DEFAULT_PERMITTED_CLASSES + [MiqExpression, MiqReport, Ruport::Data::Table, Ruport::Data::Record, User, ConfigurationScript, ContainerImage, ContainerTemplate, OpenStruct, OrchestrationTemplate, ManageIQ::Providers::Vmware::InfraManager, ManageIQ::Providers::InfraManager::Vm, ActiveModel::Type::String, ActiveModel::Attribute.const_get(:FromDatabase), ActiveModel::Attribute.const_get(:FromUser), ActiveRecord::Type::Serialized, ActiveRecord::Type::Text, ActiveRecord::Coders::YAMLColumn, ActiveModel::Type::Integer, ActiveModel::Type::Integer, VimHash, VimString, VimArray]
   end
 
   def self.default_permitted_classes
